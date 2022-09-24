56f35770356e913d0e36a608da8bf6a820d166d3
Kulakov D.A., ISTb-21-1 <dragonipam@yandex.ru>
2022-09-24 18:02:57 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+namespace task3_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
